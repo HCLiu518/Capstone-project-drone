dc527fb44fedeaed974958ab2f05d4d05f25a407
HCLiu518 <hungchih518@g.ucla.edu>
2022-08-03 15:08:25 -0700

    Fix mapping corner flying

--- Tello/mapping.py
+++ Tello/mapping.py
@@ -26,6 +26,8 @@ yaw = 0 #yaw
 kp.init()
 me = tello.Tello()
 me.connect()
+me.streamoff()
+me.streamon()
 print(me.get_battery())
 
 points = [(0,0), (0,0)]
@@ -42,24 +44,33 @@ def getKeyInput(a=0):
         lr = -speed
     #linear and angular distance change
         d = dInterval
-        a = -180
+        a = 180
     elif kp.getKey("RIGHT"):
         lr = speed
     # linear and angular distance change
-        d = -dInterval
-        a = 180
+        d = dInterval
+        a = 0
 
     # Pitch Inputs
     if kp.getKey("UP"):
         fb = speed
     # linear and angular distance change
         d = dInterval
-        a = 270
+        if kp.getKey("LEFT"):
+            a = (a + 270)/2
+        elif kp.getKey("RIGHT"):
+            a = 315
+        else:
+            a = 270
     elif kp.getKey("DOWN"):
         fb = -speed
     # linear and angular distance change
-        d = -dInterval
-        a = -90
+        d = dInterval
+        if kp.getKey("LEFT") or kp.getKey("RIGHT"):
+            a = (a + 90)/2
+        else:
+            a = 90
+
 
     # Throttle Inputs
     if kp.getKey("w"): ud = speed
@@ -82,7 +93,7 @@ def getKeyInput(a=0):
     x += int(d * math.cos(math.radians(a)))
     y += int(d * math.sin(math.radians(a)))
 
-    return [lr, fb, ud, yv, x, y], a
+    return [lr, fb, ud, yv, x, y]
 
 def drawPoints(img, points):
     for point in points:
@@ -93,10 +104,10 @@ def drawPoints(img, points):
 
 while True:
     #print(kp.getKey("s"))
-    #frame = me.get_frame_read()
+    #myFrame = me.get_frame_read()
     #myFrame = myFrame.frame
-	#img = cv2.resize(myFrame, (w, h))
-    vals, a = getKeyInput(a)
+    #myFrame = cv2.resize(myFrame, (360, 240))
+    vals = getKeyInput()
     me.send_rc_control(vals[0], vals[1], vals[2], vals[3])
 
     img = np.zeros((1000, 1000, 3), np.uint8) #2^8 = 256 0 to 255
@@ -104,5 +115,5 @@ while True:
         points.append((vals[4], vals[5]))
     drawPoints(img, points)#function draw points
     cv2.imshow("Output:", img)
-    #cv2.imshow("Result:", frame)
+    #cv2.imshow("Result:", myFrame)
     cv2.waitKey(1)
