d87db20a0676e97a48f62c38f8c3da84a2d4ca29
HCLiu518 <hungchih518@g.ucla.edu>
2022-08-09 15:35:02 -0700

    Add different color to person of interest

--- util/utility_recognition.py
+++ util/utility_recognition.py
@@ -87,19 +87,20 @@ def recognition(frame, person_of_interest):
         right = int(right)
         bottom = int(bottom)
         left = int(left)
+        color = (150,150,150)
 
-        # Draw a box around the face
-        cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
-
-        # Draw a label with a name below the face
-        cv2.rectangle(frame, (left, bottom - 10), (right, bottom), (0, 0, 255), cv2.FILLED)
-        font = cv2.FONT_HERSHEY_DUPLEX
-        cv2.putText(frame, name, (left + 6, bottom - 6), font, 0.3, (255, 255, 255), 1)
-
-        # Record POI coordinate
         if name == person_of_interest:
             poi_info[0][0] = (left + right)//2
             poi_info[0][1] = (top + bottom)//2
             poi_info[1] = (left - right)*(top - bottom)
+            color = (0,0,225)
+
+        # Draw a box around the face
+        cv2.rectangle(frame, (left, top), (right, bottom), color, 2)
+
+        # Draw a label with a name below the face
+        cv2.rectangle(frame, (left, bottom - 10), (right, bottom), color, cv2.FILLED)
+        font = cv2.FONT_HERSHEY_DUPLEX
+        cv2.putText(frame, name, (left + 6, bottom - 6), font, 0.3, (255, 255, 255), 1)
 
     return frame, poi_info
